15053f3d75a0cb26c2ae626696465146fb1621aa
PatelKrishna15 <krishahirapara666@gmail.com>
2024-01-01 13:57:22 +0530

    add script for login with register

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'google' => [
+        'client_id' => '612609179035-55htev4cli81o65gfm3po9l0dcid0jcj.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
+    ],
 
 ];
